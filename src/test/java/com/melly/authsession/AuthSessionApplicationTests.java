adede93de6c5cd119513a3ab3e30ecd85570bba0
haily <49448330+melly8954@users.noreply.github.com>
2025-08-22 15:44:51 +0900

    feat: init (250822)

---
+++ src/test/java/com/melly/authsession/AuthSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melly.authsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
